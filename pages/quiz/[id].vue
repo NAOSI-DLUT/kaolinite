06c27b0824dc6c1374a1a522b2465cd84493afed
Saurlax <saurlax@qq.com>
2024-10-08 21:10:03 +0800

    feat: update quiz page

--- pages/quiz/[id].vue
+++ pages/quiz/[id].vue
@@ -5,25 +5,44 @@ import type { Quiz } from '~/models/Quiz';
 const route = useRoute();
 const quiz = ref<Quiz>();
 const userAnswer = ref<any[]>([]);
-const currentQuestionIndex = ref(0);
+const currentQuestionIndex = ref('0');
 const questions = ref<Question<unknown>[]>([]);
 
 onMounted(async () => {
-  quiz.value = await $fetch(`/api/quiz/${route.params.id}`);
+  // TODO: wait for backend
+  // quiz.value = await $fetch(`/api/quiz/${route.params.id}`);
+  quiz.value = await $fetch(`/api/quiz`, {
+    method: 'POST',
+    body: { uid: 0 }
+  })
   if (quiz.value) {
-    for (const id of quiz.value.questions) {
-      questions.value.push(await $fetch(`/api/question/${id}`));
-      userAnswer.value.push(null);
-    }
+    // for (const id of quiz.value.questions) {
+    //   questions.value.push(await $fetch(`/api/question/${id}`));
+    //   userAnswer.value.push(null);
+    // }
+    questions.value = quiz.value;
   }
 })
+
+const submit = async () => {
+  // const result = await $fetch(`/api/quiz/${route.params.id}`, {
+  //   method: 'PUT',
+  //   body: { answers: userAnswer.value }
+  // });
+  navigateTo('/quiz');
+}
 </script>
 
 <template>
+  {{ questions }}
+  {{ currentQuestionIndex }}
   {{ userAnswer }}
   <ElTabs v-model="currentQuestionIndex">
     <ElTabPane v-for="(question, index) in questions" :key="index" :label="`第${index + 1}题`">
       <QuestionCard :question="question" v-model:userAnswer="userAnswer[index]" />
+    </ElTabPane>
+    <ElTabPane label="完成">
+      <ElButton type="primary" @click="submit">提交答案</ElButton>
     </ElTabPane>
   </ElTabs>
 </template>
