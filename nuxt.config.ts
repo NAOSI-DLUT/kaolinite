332b491fccaecd00f64c741a87fa59bd6ea19610
Saurlax <saurlax@qq.com>
2024-10-16 21:53:37 +0800

    chore: enable ssr mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   modules: ["@element-plus/nuxt", "nuxt-monaco-editor"],
-  devtools: { enabled: true },
   monacoEditor: {
     locale: "zh-hans",
   },
-  ssr: false,
   app: {
     head: {
       title: "NAOSI Quiz",
